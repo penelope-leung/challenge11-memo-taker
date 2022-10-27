85b33d5a648dbd2f8d111a6868964e01ea629901
PenPen812 <penelope.c.leung@gmail.com>
2022-10-26 19:01:34 -0500

    modified notes.json and server.js file

---
+++ main/helper/readNote.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+const NOTES_JSON_PATH = './db/notes.json';
+function readNote(callback) {
+    fs.readFile(NOTES_JSON_PATH, (err, data) => {
+        // res.json(JSON.parse(data))
+        callback(JSON.parse(data));
+    });
+}
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+function readAndAppendNote(newNote) {
+    fs.readFile(NOTES_JSON_PATH, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedData = JSON.parse(data);
+          parsedData.push(newNote);
+          writeToFile(NOTES_JSON_PATH, parsedData);
+        }
+      });
+}
+
+module.exports = {
+    readNote,
+    readAndAppendNote,
+    writeToFile,
+    NOTES_JSON_PATH
+}
---
+++ main/routes/notes.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const notes = require('express').Router();
+var { readNote, readAndAppendNote, writeToFile, NOTES_JSON_PATH } = require('../helper/readNote');
+var { v4: uuid } = require('uuid');
+
+console.log('v4: ', uuid())
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+    // fs.readFile('./db/notes.json', (err, data) => {
+    //     res.json(JSON.parse(data))
+    // });
+
+    readNote((data) => {
+        res.json(data);
+    })
+    
+//   readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new UX/UI note
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+
+  const { text, title } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    readAndAppendNote(newNote);
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+
+notes.delete('/:id', (req, res) => {
+    console.log('req', req.params.id)
+
+
+    
+    readNote(data => {
+        console.log('current', data);
+        const newArray = data.filter(note => {
+            return note.id !== req.params.id
+        });
+
+        console.log('new array', newArray)
+
+        writeToFile(NOTES_JSON_PATH, newArray)
+        res.json(); 
+    })
+  });
+
+module.exports = notes;
